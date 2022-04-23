d33a651844ff737356e32223bf16704d2942967c
faisal-kursheedali <faisal.k.ison@gmail.com>
2022-04-23 09:13:40 +0530

    add score feature

---
+++ quiz/data.js
@@ -0,0 +1,27 @@
+export default data=[
+    {
+        question:"What Indian city is the capital of two states?",
+        option:["Chennai", "Mumbai", "Chandigar", "Kolkata"],
+        ans:"Chandigar"
+    },
+    {
+        question:"How many countries border India?",
+        option:["9", "4", "3", "6"],
+        ans:"6"
+    },
+    {
+        question:"What is the capital of Gujarat?",
+        option:["Lucknow", "Bhopal", "Gandhinagar", "Mumbai"],
+        ans:"Gandhinagar"
+    },
+    {
+        question:"What is India's smallest state by area",
+        option:["Goa", "Kerala", "Uttar Pradesh", "Puducherry"],
+        ans:"Goa"
+    },
+    {
+        question:"In what state is Bengaluru located?",
+        option:["Bihar", "Kerala", "Karnataka", "Chattisgarh"],
+        ans:"Karnataka"
+    },
+]
